3361a0f381bb096d79193464fba45cfef76e8325
Cris <48873935+ckorpio@users.noreply.github.com>
2023-03-06 15:12:32 -0600

    Update expres.js

--- expres.js
+++ expres.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 
+
+//Route handler
+app.get('/hihi', function(req, res){
+   res.send("Hihi there");
+   next();
+});
+
+
 //First middleware before response is sent
 app.use(function(req, res, next){
    console.log("Start");
@@ -10,6 +18,18 @@ app.use(function(req, res, next){
 //Route handler
 app.get('/', function(req, res, next){
    res.send("Middle");
+   next();
+});
+
+//Route handler
+app.get('/hello', function(req, res, next){
+   res.send("Hello there");
+   next();
+});
+
+//First middleware before response is sent
+app.use('/somereq/:id([0-9]{5})', function(req, res, next){
+   console.log("Start");
    next();
 });
 
