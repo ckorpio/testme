2f1477c9b82479f72f34a945736143c020255ad4
Cris <48873935+ckorpio@users.noreply.github.com>
2023-03-08 21:09:09 -0600

    Update expres3.js

--- expres3.js
+++ expres3.js
@@ -1,5 +1,3 @@
-// From: https://www.digitalocean.com/community/tutorials/use-expressjs-to-get-url-and-post-parameters
-
 const express = require('express');
 
 const app = express();
@@ -42,7 +40,109 @@ app.get('/api/:version', function(req, res) {
   res.send(req.params.version);
 });
 
+// ------Step 4// From: https://www.digitalocean.com/community/tutorials/use-expressjs-to-get-url-and-post-parameters
+
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+var fs = require("fs");
+
+// ------>>>Step 5.1
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// ------<<<Step 5.1
+
+
+// routes will go here
+
+
+// ------Step 2
+
+app.get('/users', function(req, res) {
+  const myquery = req.query;
+  
+  var outstring = '';
+  for(var key in myquery) { outstring += "--" + key + ">" + myquery[key]; }
+ 
+  res.send(outstring);
+
+  const user_id = req.query.id;
+  const token = req.query.token;
+  const geo = req.query.geo;
+  res.send({
+    'user_id': user_id,
+    'token': token,
+    'geo': geo
+  });
+});
+
+
+// ------Step 2.file
+
+app.get('/wfile', function(req, res) {
+  const myquery = req.query;
+  
+  var outstring = '';
+  for(var key in myquery) { outstring += "--" + key + ">" + myquery[key]; }
+  fs.appendFile("mydata.txt", outstring+'\n', (err) => {
+    if (err)
+      console.log(err);
+    else {
+      console.log("File written successfully\n");
+      console.log("Contents of file now:\n");
+      console.log(fs.readFileSync("mydata.txt", "utf8"));
+    }
+  });
+ 
+  res.send(outstring);
+
+});
+
+
+// ------Step 3
+
+
+app.get('/api/:version', function(req, res) {
+  res.send(req.params.version);
+});
+
 // ------Step 4
+app.param('name', function(req, res, next, name) {
+  const modified = name.toUpperCase();
+
+  req.name = modified;
+  next();
+});
+
+// routes will go here
+// ...
+
+app.get('/api/users/:name', function(req, res) {
+  res.send('Hello ' + req.name + '!');
+});
+
+
+// ------>>>Step 5.2
+
+app.post('/post/users', function(req, res) {
+  const user_id = req.body.id;
+  const token = req.body.token;
+  const geo = req.body.geo;
+
+  res.send({
+    'user_id': user_id,
+    'token': token,
+    'geo': geo
+  });
+});
+// ------<<<Step 5.2
+
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
+
 app.param('name', function(req, res, next, name) {
   const modified = name.toUpperCase();
 
