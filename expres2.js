8794e2f95fca4f6a0375d1c7a1f2e797d9311edf
Cris <48873935+ckorpio@users.noreply.github.com>
2024-02-25 10:18:53 -0600

    Update expres2.js

--- expres2.js
+++ expres2.js
@@ -12,26 +12,8 @@ app.use(express.urlencoded({ extended: false }));
 
 // Routes go here
 
-
-// How to grab data from a query string (Form using GET)
-var url = require('url');
-app.get('/api/myGetData', function(req, res) {
-  var thequery = url.parse(req.url, true).query;
-  const alldata = req.query;
-  const user_id = req.query.id;
-  const token = req.query.token;
-  const geo = req.query.geo;
-
-  res.send(alldata + ' - ' + thequery);
-  res.send({
-    'user_id': user_id,
-    'token': token,
-    'geo': geo
-  });
-});
-
 // A route with a parameter (access: URI/api/someVersionString)
-app.get('/api/:version', function(req, res) {
+app.get('/api/paramData/:version', function(req, res) {
   res.send(req.params.version);
 });
 
@@ -43,9 +25,40 @@ app.param('name', function(req, res, next, name) {
 });
 
 
-// A simple route using parameter 'name'
+// A simple route using parameter 'name'(to be accessed as URI/api/users/someName)
 app.get('/api/users/:name', function(req, res) {
   res.send('Hello ' + req.name + '!');
+});
+
+
+// How to grab data from a query string (Form using GET)
+app.get('/myGetStuff', function(req, res) {
+  const myquery = req.query;
+  
+  var outstring = '';
+  for(var key in myquery) { outstring += "--" + key + ">" + myquery[key]; }
+  //);
+ 
+  res.send(outstring);
+
+});
+
+
+// -------------------------
+var url = require('url');
+app.get('api/myGetData', function(req, res) {
+  var thequery = url.parse(req.url, true).query;
+ // const alldata = req.thequery;
+  const user_id = req.query.id;
+  const token = req.query.token;
+  const geo = req.query.geo;
+
+  res.send('alldata is: ' + alldata + ' from thequery: ' + thequery);
+  res.end({
+    'user_id': user_id,
+    'token': token,
+    'geo': geo
+  });
 });
 
 
@@ -65,3 +78,22 @@ app.post('/api/users', function(req, res) {
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
+
+//----------->>> discard
+// How to grab data from a query string (Form using GET)
+var url = require('url');
+app.get('/api/myGetData2', function(req, res) {
+  var thequery = url.parse(req.url, true).query;
+  const alldata = req.query;
+  const user_id = req.query.id;
+  const token = req.query.token;
+  const geo = req.query.geo;
+
+  res.send(alldata + ' - ' + thequery);
+  res.send({
+    'user_id': user_id,
+    'token': token,
+    'geo': geo
+  });
+});
+
