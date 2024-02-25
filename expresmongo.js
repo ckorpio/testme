478f1105d43d317d2a8644874a2a9d9aa5af5ba8
Cris <48873935+ckorpio@users.noreply.github.com>
2024-02-25 13:54:39 -0600

    Update expresmongo.js

--- expresmongo.js
+++ expresmongo.js
@@ -7,10 +7,19 @@ const bodyParser=require('body-parser');
 const app = express();
 const port = 3000;
 
+// Middleware to handle jsons and URL encoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(port);
-console.log('Server started at http://localhost:' + port);
 
+// Routes go here
+
+//Default route handler
+app.get('/', function(req, res, next){
+  res.send("You did not send me anything");
+});
+
+app.get('/sdb', function(req, res){
 //---->>> From Mongo about using with Node
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://classuser:cmps415class@ckmdb.5oxvqja.mongodb.net/?retryWrites=true&w=majority";
@@ -27,68 +36,8 @@ client.connect(err => {
 });
 
 // --------<<< From Mongo
-
-
-// ------>>>Step 5.1
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-// ------<<<Step 5.1
-
-
-// routes will go here
-
-app.get('/api/:version', function(req, res) {
-  res.send(req.params.version);
-});
-
-// ------>>>Step 5.1
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-// ------<<<Step 5.1
-
-
-
-app.get('/users', function(req, res) {
-  const myquery = req.query;
-  
-  var outstring = '';
-  for(var key in myquery) { outstring += "--" + key + ">" + myquery[key]; }
- 
-  res.send(outstring);
-
-  const user_id = req.query.id;
-  const token = req.query.token;
-  const geo = req.query.geo;
-  res.send({
-    'user_id': user_id,
-    'token': token,
-    'geo': geo
-  });
 });
 
 
-// ------Step 4
-app.param('name', function(req, res, next, name) {
-  const modified = name.toUpperCase();
-
-  req.name = modified;
-  next();
-});
-
-
-
-// ------>>>Step 5.2
-
-app.post('/api/users', function(req, res) {
-  const user_id = req.body.id;
-  const token = req.body.token;
-  const geo = req.body.geo;
-
-  res.send({
-    'user_id': user_id,
-    'token': token,
-    'geo': geo
-  });
-});
-// ------<<<Step 5.2
-
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
