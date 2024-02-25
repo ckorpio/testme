689fffb822332b7b5855657211b91cdd6a261a6c
Cris <48873935+ckorpio@users.noreply.github.com>
2024-02-25 11:42:00 -0600

    Update expres3.js

--- expres3.js
+++ expres3.js
@@ -1,85 +1,31 @@
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = 3000;
+// const port = process.env.PORT || 8080;
 
 
-// ------>>>Step 5.1
+// Middleware to handle jsons and URL encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// ------<<<Step 5.1
 
 
-// routes will go here
+// Routes go here
 
-
-// ------Step 2
-
-app.get('/api/users', function(req, res) {
-  const myquery = req.query;
-  const user_id = req.query.id;
-  const token = req.query.token;
-  const geo = req.query.geo;
-  
-  var outstring = '';
-  for(var key in myquery) { outstring += "--" + key + ">" + myquery[key]; }
- 
-  res.send(outstring);
-
-  res.send({
-    'user_id': user_id,
-    'token': token,
-    'geo': geo
-  });
+//Default route handler
+app.get('/', function(req, res, next){
+  res.send("You did not send me anything");
 });
 
 
-// ------Step 3
 
-app.get('/api/:version', function(req, res) {
-  res.send(req.params.version);
-});
 
-// ------Step 4// From: https://www.digitalocean.com/community/tutorials/use-expressjs-to-get-url-and-post-parameters
-
-const express = require('express');
-
-const app = express();
-const port = 3000;
+// File system
 
 var fs = require("fs");
 
-// ------>>>Step 5.1
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-// ------<<<Step 5.1
 
-
-// routes will go here
-
-
-// ------Step 2
-
-app.get('/users', function(req, res) {
-  const myquery = req.query;
-  
-  var outstring = '';
-  for(var key in myquery) { outstring += "--" + key + ">" + myquery[key]; }
- 
-  res.send(outstring);
-
-  const user_id = req.query.id;
-  const token = req.query.token;
-  const geo = req.query.geo;
-  res.send({
-    'user_id': user_id,
-    'token': token,
-    'geo': geo
-  });
-});
-
-
-// ------Step 2.file
+// Write data obtained from a GET request into a file
 
 app.get('/wfile', function(req, res) {
   const myquery = req.query;
@@ -101,27 +47,6 @@ app.get('/wfile', function(req, res) {
 });
 
 
-// ------Step 3
-
-
-app.get('/api/:version', function(req, res) {
-  res.send(req.params.version);
-});
-
-// ------Step 4
-app.param('name', function(req, res, next, name) {
-  const modified = name.toUpperCase();
-
-  req.name = modified;
-  next();
-});
-
-// routes will go here
-// ...
-
-app.get('/api/users/:name', function(req, res) {
-  res.send('Hello ' + req.name + '!');
-});
 
 
 // ------>>>Step 5.2
@@ -137,41 +62,7 @@ app.post('/post/users', function(req, res) {
     'geo': geo
   });
 });
-// ------<<<Step 5.2
 
-
-app.listen(port);
-console.log('Server started at http://localhost:' + port);
-
-app.param('name', function(req, res, next, name) {
-  const modified = name.toUpperCase();
-
-  req.name = modified;
-  next();
-});
-
-// routes will go here
-// ...
-
-app.get('/api/users/:name', function(req, res) {
-  res.send('Hello ' + req.name + '!');
-});
-
-
-// ------>>>Step 5.2
-
-app.post('/api/users', function(req, res) {
-  const user_id = req.body.id;
-  const token = req.body.token;
-  const geo = req.body.geo;
-
-  res.send({
-    'user_id': user_id,
-    'token': token,
-    'geo': geo
-  });
-});
-// ------<<<Step 5.2
 
 
 app.listen(port);
